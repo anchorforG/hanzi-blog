fec4f289166a7b90ff0f944dc4d798837a21e7fd
anchorforG <31179617+anchorforG@users.noreply.github.com>
2024-04-30 04:40:08 +0000

    Deploying to gh-pages from  @ b6c3a8142e67b54ce3f318b0fe8634400f771e8c 🚀

---
+++ precache-manifest.18e164fecdf2306dbf16e5760e712a55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec67a0ffd8cbe067158c",
+    "url": "/css/app.2a48d19e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9d3627910d8f25d3caa65396a6f8b5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1145833d1d42959897b0",
+    "url": "/js/about.4386cea0.js"
+  },
+  {
+    "revision": "ec67a0ffd8cbe067158c",
+    "url": "/js/app.38912a87.js"
+  },
+  {
+    "revision": "45a2a542958db5c56c88",
+    "url": "/js/chunk-vendors.4e39605f.js"
+  },
+  {
+    "revision": "49f3e0598955abe280f7594f816fc3b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
